781ba0d589bc8b27ad7bd555f6cbf822b3000425
Franz Prante <35294259+franzprante@users.noreply.github.com>
2023-08-17 11:44:00 +0200

    Adds function to join raw effect size data from IRFs and JSON data for a study (without effect size transformations etc.).

---
+++ R/join_irf_json.R
@@ -0,0 +1,133 @@
+#' Join effect sizes from IRFs and JSON data for a study
+#'
+#' This function joins effect size data in the form of impulse response functions 
+#' from CSV files with the coding from a JSON file for a given study.
+#'
+#' @param key A study key identifying the study for which IRF data needs to be joined.
+#' @param jsonpath A character string specifying the path to the folder that contains 
+#'  the JSON files.
+#' @param codebook_file A character string specifiying the path to the codebook.
+#' @param irf_folder_path A character string specifying the path to the folder 
+#'  containing the IRF CSV files.
+#' @param ignore_failed_tests A logical. If TRUE, the function will parse the 
+#'  JSON despite failing consistency tests.
+#' 
+#' @return A data frame containing the joined and processed coding and IRF data.
+#'
+#' @importFrom data.table fread
+#' @importFrom here here
+#'
+#' @export
+#'
+#' @examples
+#' join_irf_json(
+#'   key = "QSKR5KKX",
+#'   jsonpath = "/Users/franzprante/GitHub/MORPEP/META_CMP/toy_data_extraction_dev/data/JSON_files",
+#'   codebook_file = "/Users/franzprante/GitHub/MORPEP/META_CMP/toy_data_extraction_dev/codebook.csv",
+#'   irf_folder_path = "/Users/franzprante/GitHub/MORPEP/META_CMP/toy_data_extraction_dev/data/effect_sizes/IRFs/",
+#'   ignore_failed_tests = FALSE)
+join_irf_json <- function(key,
+                          jsonpath = "data/JSON_files",
+                          codebook_file = "codebook.csv",
+                          irf_folder_path = "data/effect_sizes/IRFs/",
+                          ignore_failed_tests = FALSE
+                          ) {
+  
+  # Loading the coding data 
+  d_coding <- parse_study_json(json_file = paste0(key, ".json"), 
+                                              json_path = jsonpath,
+                                              codebook = codebook_file,
+                                              view_data = F,
+                                              ignore_failed_tests = ignore_failed_tests)
+  
+  # Create empty list for model level data
+  model_data <- list()
+  
+  # Identify the number of models in the study
+  models <- max(d_coding$model_id)
+  
+  # Loop through the models of the study 
+  for (i in 1:models) {
+    
+    # Select the model
+    model <- i 
+    
+    # Select model coding data
+    d_m_coding <- d_coding[d_coding$model_id == model, ]
+    row.names(d_m_coding) <- NULL
+    
+    # Extracting the list of dependent variables for the model
+    dep <- d_m_coding$dep
+    # Replace accidentally coded multiple spaces with a single space
+    dep <- gsub("\\s+", " ", dep)
+    dep <- strsplit(dep, split = " ")[[1]] 
+    
+    # Check if the subfolder "rate" exists in the model folder
+    model_folder <- paste0(irf_folder_path, key, "/model_", model)
+    rate_irf <- dir.exists(file.path(model_folder, "rate"))
+    # Add "rate" as an outcome variable if it is available
+    if (rate_irf) {
+      dep[length(dep) + 1] <- "rate"
+    }
+    
+    # Create empty list for storing of dep level data
+    data_list <- list()
+    
+    # dep variable loop
+    for (j in 1:length(dep)) {
+      
+      # Select outcome variable
+      outcome_var <- dep[j]
+      
+      # Load mean response
+      csv_path <- irf_csv_path(key, irf_folder_path, model, outcome_var, irf_part = "mean")
+      mean <- data.table::fread(csv_path, 
+                                sep = ";", 
+                                dec = ",",
+                                col.names = c("period", "mean"))
+      order_mean <- order(mean$period)
+      mean <- mean[order_mean, ]
+      # Load lower confidence band
+      csv_path <- irf_csv_path(key, irf_folder_path, model, outcome_var, irf_part = "lower")
+      lower <- data.table::fread(csv_path, 
+                                 sep = ";", 
+                                 dec = ",",
+                                 col.names = c("period", "lower"))
+      order_lower <- order(lower$period)
+      lower <- lower[order_lower, ]
+      # Load upper confidence band
+      csv_path <- irf_csv_path(key, irf_folder_path, model, outcome_var, irf_part = "upper")
+      upper <- data.table::fread(csv_path, 
+                                 sep = ";", 
+                                 dec = ",",
+                                 col.names = c("period", "upper"))
+      order_upper <- order(upper$period)
+      upper <- upper[order_upper, ]
+      
+      # Create IRF dataframe for the dep variable
+      dep_data <- data.frame(
+        period = 1:nrow(mean), # This standardizes all periods for all studies such that the first period is always period 1.
+        CI.upper.raw = upper$upper,
+        mean.effect.raw = mean$mean,
+        CI.lower.raw = lower$lower,
+        outcome_var = outcome_var
+      )
+      
+      # Store in data_list
+      data_list[[j]] <- cbind(d_m_coding[,-which(names(d_m_coding) == "dep")],dep_data)
+      
+      # Clean up
+      rm(upper, mean, lower, dep_data)
+    }
+    
+    # rbind the dep-level dataframes
+    model_data[[i]] <- do.call(rbind, data_list)
+    
+  }
+  
+  # rbind the model-level dataframes
+  study_data <- do.call(rbind, model_data)
+  
+  study_data
+  
+}
