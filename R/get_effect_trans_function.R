38e5d0d84babd36b5d5becdea71a107dd5b6e275
Franz Prante <35294259+franzprante@users.noreply.github.com>
2023-08-17 14:32:24 +0200

    Adds helper function that creates functions for effect size standardization and standard error approximation.

---
+++ R/get_effect_trans_function.R
@@ -0,0 +1,59 @@
+#' Create Effect Size Transformation and SE Functions for the Respective Case
+#'
+#' This function creates a function called `trans_function()` that represents 
+#' the correct transformation function and a function called `se_function()` that
+#' represents the correct standard error approximation based on the provided 
+#' observation d from a dataframe created via the `join_irf_json()` function.
+#'
+#' @param d An observation (i.e. a row) from a dataframe obtained using `join_irf_json()`.
+#'
+#' @return A list containing a function called `trans_function()` an a function 
+#'  called `se_function()` as elements.
+#'   
+#' @examples
+#' # Assuming d is given:
+#' trans_function <- get_effect_trans_function(d)$trans_function
+#' se_function <- get_effect_trans_function(d)$se_function
+#' 
+#' @export
+get_effect_trans_function <- function (d) {
+
+  # Get specifications of shock and outcome variable
+  shock_code <- d$inttype
+  dep_code <- d$outcome_var
+  cum <- d$cum
+  
+  if (grepl("log_", dep_code) & grepl("lev_", shock_code) & cum == FALSE) {
+    
+    # If case 1: Log-level of response variable and level of shock variable, non-cumulative
+    trans_function <- function (x) x / shock_size * 100 # case 1 function
+    se_function <- function (x) x # PLACEHOLDER TO DO: replace with case 1 SE function 
+    
+  } else if (grepl("gr_", dep_code) & grepl("lev_", shock_code) & cum == FALSE) {
+    
+    # If case 2: Growth rate of response variable and level of shock variable, non-cumulative
+    trans_function <- function (x) x # PLACEHOLDER TO DO: replace with case 2 function
+    se_function <- function (x) x # PLACEHOLDER TO DO: replace with case 2 SE function 
+
+  } else if (grepl("logdiff_", dep_code) & grepl("lev_", shock_code)  & cum == FALSE) {
+    
+    # If case 3: Log-difference of response variable and level of shock variable, non-cumulative
+    trans_function <- function (x) x # PLACEHOLDER TO DO: replace with case 3 function 
+    se_function <- function (x) x # PLACEHOLDER TO DO: replace with case 3 SE function 
+    
+  } else if (grepl("logdiff_", dep_code) | grepl("gr_", dep_code) & grepl("lev_", shock_code)  & cum == TRUE) {
+    
+    # If case 5: Log-difference or growth rate of response variable and level of shock variable, cumulative
+    trans_function <- function (x) x # PLACEHOLDER TO DO: replace with case 5 function 
+    se_function <- function (x) x # PLACEHOLDER TO DO: replace with case 5 SE function 
+    
+  }
+  
+  l <- list(
+    "trans_function" = trans_function,
+    "se_function" = se_function
+    )
+  
+  return(l)
+  
+}
