f28f07cb4d24b176c0ada4d55ea1df1aa0fec9c2
Franz Prante <35294259+franzprante@users.noreply.github.com>
2023-08-21 14:12:12 +0200

    Adds helper function to adjust the periodicity of the BP shock size.

---
+++ R/adjust_shock_periodicity.R
@@ -0,0 +1,39 @@
+#' Adjust shock_size based on the periodicity of interest rate.
+#'
+#' This function adjusts the shock size based on the periodicity of the interest rate.
+#'
+#' @param d An observation (i.e., a row) from a dataframe obtained using MetaExtractR::join_irf_json().
+#'
+#' @return Annualized shock size.
+#'
+#' @export
+adjust_shock_periodicity <- function (d) {
+  
+  if (grepl("_a_", periodicity) == TRUE) {
+    
+    # No transformation necessary
+    message("Annualized interest rate. No periodicity transformation necessary for shock size.")
+    shock_size <- shock_size
+    
+  } else if (grepl("_q_", periodicity) == TRUE) {
+    
+    # If quarterly periodicity, transform to annualized
+    shock_size <- ( (1 + (shock_size/10000) )^4 - 1)*10000
+    
+  } else if (grepl("_m_", periodicity) == TRUE) {
+    
+    # If monthly periodicity, transform to annualized
+    shock_size <- ((1+(shock_size/10000))^12-1)*10000
+    
+  }
+  
+  return(shock_size)
+  
+}
+  
+# TO DO: 
+  # Comment ME: 
+  # but how to handle the interest rate response since this one should be
+  # multiplied by the same factor and not divided?
+  # -------------------------------------------------------------------------------
+    
