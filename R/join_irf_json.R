c0bc7d93f5268314b9760ceece9d9f171c028d67
Franz Prante <35294259+franzprante@users.noreply.github.com>
2024-04-03 10:58:10 +0200

    Debugging #14

--- R/join_irf_json.R
+++ R/join_irf_json.R
@@ -138,14 +138,12 @@ join_irf_json <- function(key,
     # Automatic IRF checks
     # IRF test: Check that for a specific model, the IRFs for all dependent variables have the same number of observations
       # Step 1: Split the dataframe by 'outcome_var'
-      split_data <- split(model_data[[i]], df$outcome_var)
+      split_data <- split(model_data[[i]], model_data[[i]]$outcome_var)
       # Step 2: Get the number of rows (i.e. periods) for each 'outcome_var'
       periods_per_outcome_var <- lapply(split_data, nrow)
       # Step 3: Check if all 'outcome_var' have the same number of periods
       if (length(unique(periods_per_outcome_var)) != 1) {
         stop("IRF test failed: Not all dep IRFs have the same number of periods. Check IRF extraction. ", key, " model_", i)
-      } else {
-        message("IRF test successful: All dep IRFs have the same number of periods.")
       }
     # IRF test: Check if less than 8 observations for quarterly data or less than 16 for monthly data
       # Check for quarterly
@@ -173,4 +171,3 @@ join_irf_json <- function(key,
   study_data
   
 }
-
